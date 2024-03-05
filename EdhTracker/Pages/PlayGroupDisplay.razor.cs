4d5138c1b467ed378af58d6c7b1a89b1a09be5b9
XenotropicDev <eliotd@gmail.com>
2024-03-05 07:25:26 -0800

    feature: initial play group display/ui

--- EdhTracker/Pages/PlayGroupDisplay.razor.cs
+++ EdhTracker/Pages/PlayGroupDisplay.razor.cs
@@ -4,34 +4,11 @@ using Microsoft.EntityFrameworkCore;
 using MudBlazor;
 
 namespace EdhTracker.Pages;
+
 public partial class PlayGroupDisplay : IDisposable
 {
-    protected override async Task OnInitializedAsync()
-    {
-        DataContext = await DataContextFactory.CreateDbContextAsync();
-    }
-
-    private DataContext DataContext;
-
-    [Parameter]
-    public Guid PodId { get; set; }
-
-    [Inject]
-    public IDbContextFactory<DataContext> DataContextFactory { get; set; }
-
-    [Inject]
-    public IDialogService DialogService { get; set; }
-
-    public PlayGroup Pod { get => DataContext.Pods.Find(PodId)!; }
-
     public void Dispose()
     {
         DataContext.Dispose();
     }
-
-    public void OpenAddGameDialog()
-    {
-        var options = new DialogOptions { CloseOnEscapeKey = true };
-    }
-
 }
